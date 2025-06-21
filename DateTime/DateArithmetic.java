05332d8c6d0806ff66dfec0fced417ab5eea2ee6
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-06-21 22:36:58 +0530

    Performing arithmetic operations on input date by user

--- DateTime/DateArithmetic.java
+++ DateTime/DateArithmetic.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 
 public class DateArithmetic {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter date (yyyy-MM-dd): ");
-        String input = scanner.nextLine();
+        String input = sc.nextLine();
 
         LocalDate date = LocalDate.parse(input);
         LocalDate modifiedDate = date.plusDays(7).plusMonths(1).plusYears(2).minusWeeks(3);
 
         System.out.println("Original Date: " + date);
         System.out.println("Modified Date: " + modifiedDate);
+
+        sc.close();
     }
 }
 
