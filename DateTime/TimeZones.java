b05bfbdbf5195be39128c090cf84ac4d23aa8781
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-06-23 08:32:02 +0530

    [SnehaAgarwal]Add .  Printing the time of different zones

--- DateTime/TimeZones.java
+++ DateTime/TimeZones.java
@@ -1,7 +1,6 @@
 import java.time.ZonedDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-
 public class TimeZones {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss z");
