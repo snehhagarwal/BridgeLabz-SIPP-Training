658527204da01addec496615a7fa5459d8c698e0
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-06-23 08:30:05 +0530

    [SnehaAgarwal]Add . Finding the maximum of three numbers.

--- DateTime/MaxOfThree.java
+++ DateTime/MaxOfThree.java
@@ -1,5 +1,4 @@
 import java.util.*; // Import utility classes
-
 public class MaxOfThree {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // Scanner for input
