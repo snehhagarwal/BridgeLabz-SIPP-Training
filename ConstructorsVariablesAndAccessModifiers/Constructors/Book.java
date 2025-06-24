cee30f289e132ef0416230061b7ccf53876e9f25
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-06-24 11:26:57 +0530

    [Sneha Agarwal]. Add book containing both default and parameterised constructor

---
+++ ConstructorsVariablesAndAccessModifiers/Constructors/Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    // Default constructor
+    public Book() {
+        this.title = "Unknown Title";
+        this.author = "Unknown Author";
+        this.price = 0.0;
+    }
+
+    // Parameterized constructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Method to display book details
+    public void displayBookDetails() {
+        System.out.println("Title : " + title);
+        System.out.println("Author: " + author);
+        System.out.printf("Price : ₹%.2f\n", price);
+    }
+}
