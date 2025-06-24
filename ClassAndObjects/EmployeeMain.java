a2c503329059600e6d9be9e854694bea38868b2b
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-06-24 09:12:00 +0530

    [Sneha Agarwal]Add .Item file to calculate total cost of item according to quantity

--- ClassAndObjects/EmployeeMain.java
+++
@@ -1,9 +0,0 @@
-public class EmployeeMain {
-    public static void main(String[] args) {
-        // Create an Employee object
-        Employee emp = new Employee("Sneha Agarwal", 101, 50000.00);
-
-        // Display employee details
-        emp.displayDetails();
-    }
-}
