26d596eb142d2173b0f7be014e1e57e1d2b90857
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-06-25 08:55:28 +0530

    [Sneha Agarwal]. Add file to display the details of book and to set and get author

---
+++ ConstructorsVariablesAndAccessModifiers/AccessModifiers/Book.java
@@ -0,0 +1,32 @@
+package ConstructorsVariablesAndAccessModifiers.AccessModifiers;
+
+public class Book {
+    public String ISBN;
+    protected String title;
+    private String author;
+
+    // Constructor
+    public Book(String ISBN, String title, String author) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getter for author
+    public String getAuthor() {
+        return author;
+    }
+
+    // Setter for author
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Method to display book info
+    public void displayBookDetails() {
+        System.out.println("ISBN   : " + ISBN);
+        System.out.println("Title  : " + title);
+        System.out.println("Author : " + author);
+    }
+}
+
