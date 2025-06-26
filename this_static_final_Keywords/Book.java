04e0c5c0be5db25194312a33b30f9149a2c5ff57
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-06-26 08:38:41 +0530

    [Sneha Agarwal]. Add file to display the book details and library name

---
+++ this_static_final_Keywords/Book.java
@@ -0,0 +1,25 @@
+package this_static_final_Keywords;
+
+public class Book {
+    public static String libraryName = "Central City Library";
+
+    private String title;
+    private String author;
+    private final String isbn;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public static void displayLibraryName() {
+        System.out.println("Library Name: " + libraryName);
+    }
+
+    public void displayBookDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("ISBN: " + isbn);
+    }
+}
