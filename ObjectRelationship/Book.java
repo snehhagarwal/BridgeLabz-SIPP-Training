e16702d5efdb92d15eddd5079c0398d81f489309
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-06-27 08:16:09 +0530

    [Sneha Agarwal]. Add file to get title, author and display both

---
+++ ObjectRelationship/Book.java
@@ -0,0 +1,25 @@
+package ObjectRelationship;
+
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String toString() {
+        return "\"" + title + "\" by " + author;
+    }
+}
+
