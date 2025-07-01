b2ab4ba1428706f8e5855e977adbf089f0ce0bee
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-01 11:40:20 +0530

    [Sneha Agarwal]. Add file to display the details of book

---
+++ Inheritance/Book.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class Book {
+    protected String title;
+    protected int publicationYear;
+
+    public Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    public void displayInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Publication Year: " + publicationYear);
+    }
+}
+
