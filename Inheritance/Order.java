d6620b9b6b41bac22032b258d6f7744b5d237aa3
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-01 11:53:49 +0530

    [Sneha Agarwal]. Add file to get the order status

---
+++ Inheritance/Order.java
@@ -0,0 +1,22 @@
+package Inheritance;
+
+public class Order {
+    protected String orderId;
+    protected String orderDate;
+
+    public Order(String orderId, String orderDate) {
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return "Order placed";
+    }
+
+    public void displayOrderDetails() {
+        System.out.println("Order ID: " + orderId);
+        System.out.println("Order Date: " + orderDate);
+        System.out.println("Status: " + getOrderStatus());
+    }
+}
+
