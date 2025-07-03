f466657ce4fbbeecfef52f5235314d657ddcfadd
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-03 08:26:17 +0530

    [Sneha Agarwal]. Add file for manager that inherits employee

---
+++ Inheritance/Manager.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class Manager extends Employee {
+    private int teamSize;
+
+    public Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Team Size: " + teamSize);
+    }
+}
+
