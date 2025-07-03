1624864950b56ce5d4f0be7139d5228bf2394f5c
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-03 09:58:12 +0530

    [Sneha Agarwal]. Add file for full time employee details

---
+++ Polymorphism/FullTimeEmployee.java
@@ -0,0 +1,27 @@
+package Polymorphism;
+
+public class FullTimeEmployee extends Employee implements Department {
+    private String department;
+    private double bonus;
+
+    public FullTimeEmployee(int employeeId, String name, double baseSalary, double bonus) {
+        super(employeeId, name, baseSalary);
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return getBaseSalary() + bonus;
+    }
+
+    @Override
+    public void assignDepartment(String deptName) {
+        this.department = deptName;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return "Department: " + department;
+    }
+}
+
