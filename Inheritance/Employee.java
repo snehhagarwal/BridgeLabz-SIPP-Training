6c74b0c5153d9d10d3f560ba20a3dad3dd482d73
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-03 08:25:38 +0530

    [Sneha Agarwal]. Add file for employee details

---
+++ Inheritance/Employee.java
@@ -0,0 +1,19 @@
+package Inheritance;
+
+public class Employee {
+    protected String name;
+    protected int id;
+    protected double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: ₹" + salary);
+    }
+}
