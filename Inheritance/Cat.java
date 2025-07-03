66b7cf6d259ef5ff234365d596116c3d1b056a94
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-03 08:22:25 +0530

    [Sneha Agarwal]. Add file for cat sound

---
+++ Inheritance/Cat.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class Cat extends Animal {
+
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow! Meow!");
+    }
+}
