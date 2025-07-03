47c66ff4a54cccaed0b9f06269a623cb8a615825
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-03 08:29:28 +0530

    [Sneha Agarwal]. Add file for intern that extends employee

---
+++ Inheritance/Intern.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class Intern extends Employee {
+    private String university;
+
+    public Intern(String name, int id, double salary, String university) {
+        super(name, id, salary);
+        this.university = university;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("University: " + university);
+    }
+}
+
