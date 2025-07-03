8c4baa47fa35235598b71e4226164650ce58d934
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-03 10:09:56 +0530

    [Sneha Agarwal]. Add file for clothing shopping

---
+++ Polymorphism/ECommerce/Groceries.java
@@ -0,0 +1,13 @@
+package Polymorphism.ECommerce;
+
+public class Groceries extends Product {
+    public Groceries(int productId, String name, double price) {
+        super(productId, name, price);
+    }
+
+    @Override
+    public double calculateDiscount() {
+        return getPrice() * 0.05; // 5% discount
+    }
+}
+
