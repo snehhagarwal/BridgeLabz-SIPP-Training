507940cc096549324bfeb875a883a119f16d196a
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-03 10:19:59 +0530

    [Sneha Agarwal]. Add file for library item details

---
+++ Polymorphism/LibraryManagement/LibraryItem.java
@@ -0,0 +1,34 @@
+package Polymorphism.LibraryManagement;
+
+public abstract class LibraryItem {
+    private String itemId;
+    private String title;
+    private String author;
+
+    public LibraryItem(String itemId, String title, String author) {
+        this.itemId = itemId;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void getItemDetails() {
+        System.out.println("Item ID: " + itemId);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
+
+    public abstract int getLoanDuration(); // in days
+}
+
