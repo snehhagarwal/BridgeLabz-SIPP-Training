92cfe45677da30b5b216b9c3e0654d404bf8d3df
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-03 10:25:18 +0530

    [Sneha Agarwal]. Add file for food item details

---
+++ Polymorphism/OnlineFood/FoodItem.java
@@ -0,0 +1,34 @@
+package Polymorphism.OnlineFood;
+
+public abstract class FoodItem {
+    private String itemName;
+    private double price;
+    private int quantity;
+
+    public FoodItem(String itemName, double price, int quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void getItemDetails() {
+        System.out.println("Item: " + itemName);
+        System.out.println("Price per Unit: ₹" + price);
+        System.out.println("Quantity: " + quantity);
+    }
+
+    public abstract double calculateTotalPrice(); // To be overridden
+}
+
