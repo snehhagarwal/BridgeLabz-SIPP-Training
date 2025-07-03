1b499ebaa2ba8838c143e8c4802c8426529dd2fd
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-03 09:59:13 +0530

    [Sneha Agarwal]. Add file for part time employee details

---
+++ Polymorphism/PartTimeEmployee.java
@@ -0,0 +1,29 @@
+package Polymorphism;
+
+public class PartTimeEmployee extends Employee implements Department {
+    private String department;
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(int employeeId, String name, double hourlyRate, int hoursWorked) {
+        super(employeeId, name, 0); // baseSalary unused for part-time
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hourlyRate * hoursWorked;
+    }
+
+    @Override
+    public void assignDepartment(String deptName) {
+        this.department = deptName;
+    }
+
+    @Override
+    public String getDepartmentDetails() {
+        return "Department: " + department;
+    }
+}
+
