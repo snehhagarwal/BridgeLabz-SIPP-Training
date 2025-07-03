b63414dcd1dccfc702c9a2c01307fba5b2758e53
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-03 10:06:53 +0530

    [Sneha Agarwal]. Add file for product details

---
+++ Polymorphism/ECommerce/Product.java
@@ -0,0 +1,38 @@
+package Polymorphism.ECommerce;
+
+public abstract class Product {
+    private int productId;
+    private String name;
+    private double price;
+
+    public Product(int productId, String name, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public abstract double calculateDiscount();
+
+    public void displayInfo() {
+        System.out.println("Product ID: " + productId);
+        System.out.println("Name: " + name);
+        System.out.println("Price: ₹" + price);
+    }
+}
+
