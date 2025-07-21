05db5b07338e0947ea68b16287d82bc88d0d08c2
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-21 09:56:43 +0530

    [Sneha Agarwal]. Add for implement binary search to find peak element in unsorted array

---
+++ Searching/PeakElement.java
@@ -0,0 +1,22 @@
+package Searching;
+
+public class PeakElement {
+    public static int findPeak(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return arr[left];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 20, 4, 1, 0};
+        System.out.println(findPeak(arr));
+    }
+}
+
