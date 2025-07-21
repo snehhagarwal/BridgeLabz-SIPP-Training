0386b77f0d2ef51ddead73e73133d638dcffb46e
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-21 11:07:55 +0530

    [Sneha Agarwal]. Add Meal<T> class and generic method for meal plan validation.

--- Day1/InvalidQuizSubmissionException.java
+++
@@ -1,8 +0,0 @@
-package Day1;
-
-public class InvalidQuizSubmissionException extends Exception {
-    public InvalidQuizSubmissionException(String message) {
-        super(message);
-    }
-}
-
--- Day2/CinemaBooking/InvalidTimeFormatException.java
+++
@@ -1,7 +0,0 @@
-package Day2.CinemaBooking;
-
-public class InvalidTimeFormatException extends Exception {
-    public InvalidTimeFormatException(String message) {
-        super(message);
-    }
-}
