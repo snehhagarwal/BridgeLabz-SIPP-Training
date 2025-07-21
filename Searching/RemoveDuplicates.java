aa062c110b3bc28a1401cc8f96e0ab691cc58891
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-07-21 09:57:29 +0530

    [Sneha Agarwal]. Add for removing duplicates from an array

---
+++ Searching/RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package Searching;
+
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+    public static String removeDuplicates(String input) {
+        StringBuilder sb = new StringBuilder();
+        HashSet<Character> seen = new HashSet<>();
+
+        for (char ch : input.toCharArray()) {
+            if (!seen.contains(ch)) {
+                seen.add(ch);
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "programming";
+        System.out.println(removeDuplicates(input));
+    }
+}
+
